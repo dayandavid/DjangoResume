f179c342bca835949da5abe3ad09805309427eb2
Dayan David <ddavidcoder@gmail.com>
2020-10-09 12:32:11 -0700

    Included the project app in the installed apps in the settings.

--- DjangoTestingProject/settings.py
+++ DjangoTestingProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DjangoTestingApp.apps.DjangotestingappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
