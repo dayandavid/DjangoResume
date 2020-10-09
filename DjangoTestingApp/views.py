86ed509f23aaa6da96052c5be094ba57bc6dd4b7
Dayan David <ddavidcoder@gmail.com>
2020-10-09 15:53:44 -0700

    Adding translation to the data sended to the view.

--- DjangoTestingApp/views.py
+++ DjangoTestingApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+
 
 def index(request):
-    message = 'Hello World Message'
+    message = _('Hello World')
     
     return render(request, 'DjangoTestingApp/index.html', {'helloWorld': message})
