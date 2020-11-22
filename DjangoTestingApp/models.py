81ff6368f083cf7c7f180599b399c1f49ee9cff1
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-22 00:51:10 -0800

    Created models Job Position and Job Experience

--- DjangoTestingApp/models.py
+++ DjangoTestingApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class JobPosition(models.Model):
+    """
+    A model that describes a job position in a company.
+    """
+    position_name = models.CharField(max_length=50)
+
+
+class JobExperience(models.Model):
+    """
+    A model that describes a job experience. Includes the position,
+    the name of the company and the amount of time you where there.
+    """
+    
+    job_position = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    company_description = models.TextField()
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    current_company = models.BooleanField()
