a60e03abf3ca051c9b896c2a109fe697b4987be6
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-22 19:10:35 -0800

    Created models profile and Social Links.

--- DjangoTestingApp/models.py
+++ DjangoTestingApp/models.py
@@ -5,6 +5,10 @@ class JobPosition(models.Model):
     A model that describes a job position in a company.
     """
     position_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.position_name
+    
 
 
 class JobExperience(models.Model):
@@ -13,9 +17,40 @@ class JobExperience(models.Model):
     the name of the company and the amount of time you where there.
     """
     
-    job_position = models.CharField(max_length=50)
+    job_position = models.ForeignKey(JobPosition, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=50)
     company_description = models.TextField()
-    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    end_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False, null=True)
     current_company = models.BooleanField()
+    
+    def __str__(self):
+        return self.company_name
+
+
+class Profile(models.Model):
+    """
+    A model that describes the profile of the user. Name, mail and description.
+    """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+    email = models.EmailField(max_length=254)
+    description = models.TextField()
+    profile_pic = models.ImageField(upload_to='profileImages', height_field=None, width_field=None, max_length=None, verbose_name='Profile Picture')
+    
+    def __str__(self):
+        return self.first_name
+
+
+class SocialLink(models.Model):
+    """
+    A model that describes the link to the social media and the names of the social media.
+    """
+    name = models.CharField(max_length=50)
+    url = models.URLField(max_length=200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
