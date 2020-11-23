83e667dbdab90583d1e03c339120ce7424df4cb0
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-22 22:40:21 -0800

    Added the media files to the url configuration.

--- DjangoTestingProject/urls.py
+++ DjangoTestingProject/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+
 from DjangoTestingApp import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
