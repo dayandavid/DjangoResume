0e74fa02b1763e2bc483db27a17501d1d4f35dde
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-22 19:11:57 -0800

    Sending data from profile job experiences and social links to the frontend.

--- DjangoTestingApp/views.py
+++ DjangoTestingApp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-
+from .models import Profile, JobExperience, SocialLink
 
 def index(request):
-    message = _('Hello World')
+    profile = Profile.objects.first()
+    jobExperiences = JobExperience.objects.all()
+    social_links = SocialLink.objects.all()
     
-    return render(request, 'DjangoTestingApp/index.html', {'helloWorld': message})
+    context = {
+        'profile': profile,
+        'job_experiences': jobExperiences,
+        'social_links': social_links
+        }
+    
+    return render(request, 'DjangoTestingApp/index.html', context)
