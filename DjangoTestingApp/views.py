fbdc7ed983f42a769884f26ea541e3f957bbf4fa
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-24 13:05:55 -0800

    Sending the education records to thru the views.

--- DjangoTestingApp/views.py
+++ DjangoTestingApp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from .models import Profile, JobExperience, SocialLink
+from .models import Profile, JobExperience, SocialLink, Education
 
 def index(request):
     profile = Profile.objects.first()
     job_experiences = JobExperience.objects.all()
     social_links = SocialLink.objects.all()
+    educations = Education.objects.all()
     
     context = {
         'profile': profile,
         'job_experiences': job_experiences,
-        'social_links': social_links
+        'social_links': social_links,
+        'educations': educations
         }
     
     return render(request, 'DjangoTestingApp/index.html', context)
