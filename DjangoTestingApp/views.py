af84c5977b15573318c025f5e18051657380f563
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-24 21:26:03 -0800

    Sending the skills data to the frontend.

--- DjangoTestingApp/views.py
+++ DjangoTestingApp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from .models import Profile, JobExperience, SocialLink, Education
+from .models import Profile, JobExperience, SocialLink, Education, Skill
 
 def index(request):
     profile = Profile.objects.first()
     job_experiences = JobExperience.objects.all()
     social_links = SocialLink.objects.all()
     educations = Education.objects.all()
+    skills = Skill.objects.all()
     
     context = {
         'profile': profile,
         'job_experiences': job_experiences,
         'social_links': social_links,
-        'educations': educations
+        'educations': educations,
+        'skills': skills
         }
     
     return render(request, 'DjangoTestingApp/index.html', context)
