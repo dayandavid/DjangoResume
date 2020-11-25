0593dd58938d6a4d30fcbad7c9e7d4ca2decef4a
dayandavid <33235380+dayandavid@users.noreply.github.com>
2020-11-24 21:39:48 -0800

    Sending the workflow data to the frontend.

--- DjangoTestingApp/views.py
+++ DjangoTestingApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from .models import Profile, JobExperience, SocialLink, Education, Skill
+from .models import Profile, JobExperience, SocialLink, Education, Skill, Workflow
 
 def index(request):
     profile = Profile.objects.first()
@@ -8,13 +8,15 @@ def index(request):
     social_links = SocialLink.objects.all()
     educations = Education.objects.all()
     skills = Skill.objects.all()
+    workflows = Workflow.objects.all()
     
     context = {
         'profile': profile,
         'job_experiences': job_experiences,
         'social_links': social_links,
         'educations': educations,
-        'skills': skills
+        'skills': skills,
+        'workflows': workflows
         }
     
     return render(request, 'DjangoTestingApp/index.html', context)
